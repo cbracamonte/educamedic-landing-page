13b223d40026d445d6e413004c63a11752e57d87
Hernan <carlosbracamonte23@gmail.com>
2024-10-23 00:11:18 -0500

    pwa edit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,26 +1,20 @@
 import { ApplicationConfig, isDevMode } from "@angular/core";
 import {
-  NavigationError,
   provideRouter,
-  withDebugTracing,
   withInMemoryScrolling,
-  withNavigationErrorHandler,
   withRouterConfig,
 } from "@angular/router";
 
 import { routes } from "./app.routes";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import {
-  HTTP_INTERCEPTORS,
   provideHttpClient,
   withInterceptors,
 } from "@angular/common/http";
 import { ENVIRONMENT } from "./core/tokens/environment.token";
 import { environment } from "../environments/environment";
 import { HttpToastInterceptor } from "./core/interceptors/http-toast.interceptor";
-import { provideServiceWorker } from '@angular/service-worker';
-// import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
-// import { HttpInterceptorService } from 'src/interceptors/authorize.interceptor';
+import { provideServiceWorker } from "@angular/service-worker";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -39,20 +33,10 @@ export const appConfig: ApplicationConfig = {
       })
     ),
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptors([HttpToastInterceptor])), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }),
-    
-    // provideToastr(),
-    // provideHttpClient(
-    //   withInterceptorsFromDi() // If using non-functional interceptors
-    //   // withInterceptors([cookieCredentialsInterceptor, authorizeInterceptor]) if using functional interceptors
-    // ),
-    // {
-    //   provide: HTTP_INTERCEPTORS,
-    //   useClass: HttpInterceptorService,
-    //   multi: true,
-    // },
+    provideHttpClient(withInterceptors([HttpToastInterceptor])),
+    provideServiceWorker("ngsw-worker.js", {
+      enabled: isDevMode(),
+      registrationStrategy: "registerWhenStable:30000",
+    }),
   ],
 };
